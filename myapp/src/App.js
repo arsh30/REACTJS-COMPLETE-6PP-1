05f42cac9656b7f36af05653b16594ea4c540859
Arsh <as3096734@gmail.com>
2022-11-09 22:11:10 +0530

    upto 1hr

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,24 @@
-import Heading from "./Heading";
+import Header from "./Components/Header";
+import "./styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+
 function App() {
   return (
     <Router>
       {/* ROUTES ISKE BAHAR VO CHEEZE RAKHEGE JO HAR PAGE PR SHOW  
        KRWANI H */}
+      <Header />
       <Routes>
-        {/* <Route path="/" element={<Heading />} /> */}
-        {/* so jb jb heading pr click hoga toh yeh open hojega 
-         path */}
+        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Home />} />
+        <Route path="/about" element={<Contact />} />
+        <Route path="/product:id" element={<Contact />} />
+        {/* Dynamci routing ->  */}
+        <Route path="*" element={<PageNotFound />} />
+        {/* * -> used to make 404 page */}
       </Routes>
     </Router>
   );
